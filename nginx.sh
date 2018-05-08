d102822eba51761a11a08c24d953da6364cbe1ba
kondo takeshi <take.she12@gmail.com>
2018-05-09 01:57:40 +0900

    nginx実践入門 3.1 設定ファイルの構成
    * don't specify access_log in nginx.conf
    * change root directory

--- nginx.sh
+++ nginx.sh
@@ -2,4 +2,7 @@
 cp /vagrant/nginx.repo /etc/yum.repos.d/
 yum update -y
 yum install -y nginx
+cp -f /vagrant/nginx.conf /etc/nginx/
+mkdir -p /var/www/html
+cp /vagrant/index.html /var/www/html/
 nginx
