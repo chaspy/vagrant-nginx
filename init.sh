701d0108d53153e3cd4541966a190dfc6b6081ec
kondo takeshi <take.she12@gmail.com>
2018-05-09 23:51:26 +0900

    add error page

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ yum update -y
 yum install -y nginx
 cp -f /vagrant/nginx.conf /etc/nginx/
 mkdir -p /var/www/html
-cp /vagrant/index.html /var/www/html/
+cp /vagrant/*.html /var/www/html/
 systemctl start nginx.service
