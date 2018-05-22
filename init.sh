8d11cb36cbc235f317271efa91435f1163562a0a
kondo takeshi <take.she12@gmail.com>
2018-05-22 13:47:35 +0900

    add ngx_lua

--- init.sh
+++ init.sh
@@ -2,15 +2,21 @@
 VERSION=1.14.0
 
 yum update -y
+yum install -y git
 yum install -y gcc
 yum install -y pcre-devel openssl-devel libxslt-devel gd-devel perl-ExtUtils-Embed epel-release
 yum install -y GeoIP-devel
+yum install -y lua-devel
 
 cd /usr/local/src/
+# for ngx_lua
+git clone https://github.com/openresty/lua-nginx-module.git
+git clone https://github.com/simpl/ngx_devel_kit.git
+
 curl -L -O http://nginx.org/download/nginx-$VERSION.tar.gz
 tar zxvf ./nginx-$VERSION.tar.gz
 cd nginx-$VERSION
-./configure --prefix=/usr/local/nginx --sbin-path=/usr/local/nginx
+./configure --prefix=/usr/local/nginx --sbin-path=/usr/local/nginx --add-module=/usr/local/src/lua-nginx-module --add-module=/usr/local/src/ngx_devel_kit
 make
 sudo make install
 echo "export PATH=/usr/local/nginx/:$PATH" >> /etc/profile.d/nginx.sh
