5d0ea55b901d9cac142edab0fa3a6164e8e77a1b
kondo takeshi <take.she12@gmail.com>
2018-05-22 10:02:43 +0900

    build from source code

--- init.sh
+++ init.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-cp /vagrant/nginx.repo /etc/yum.repos.d/
+VERSION=1.14.0
+
 yum update -y
-yum install -y nginx
-cp -f /vagrant/nginx.conf /etc/nginx/
+yum install -y gcc
+yum install -y pcre-devel openssl-devel libxslt-devel gd-devel perl-ExtUtils-Embed epel-release
+yum install -y GeoIP-devel
+
+cd /usr/local/src/
+curl -L -O http://nginx.org/download/nginx-$VERSION.tar.gz
+tar zxvf ./nginx-$VERSION.tar.gz
+cd nginx-$VERSION
+./configure --prefix=/usr/local/nginx --sbin-path=/usr/local/nginx
+make
+sudo make install
+echo "export PATH=/usr/local/nginx/:$PATH" >> /etc/profile.d/nginx.sh
+source /etc/profile
+
+cp -f /vagrant/nginx.conf /usr/local/nginx/conf/
 mkdir -p /var/www/html
+mkdir -p /var/log/nginx
 cp /vagrant/*.html /var/www/html/
-systemctl start nginx.service
+
+touch /var/log/nginx/www.example.com_access.log
+touch /var/log/nginx/www.example.com_error.log
+
+nginx
