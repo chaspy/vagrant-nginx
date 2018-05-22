96b55cf4fb6b96580bddcdb8751ca58d4e3a06e8
kondo takeshi <take.she12@gmail.com>
2018-05-22 15:59:02 +0900

    add nginx_mruby module

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 VERSION=1.14.0
+NGINX_SRC=/usr/local/src/nginx-$VERSION
+NGX_MRUBY_SRC=/usr/local/src/ngx_mruby
 
 yum update -y
 yum install -y git
@@ -7,18 +9,30 @@ yum install -y gcc
 yum install -y pcre-devel openssl-devel libxslt-devel gd-devel perl-ExtUtils-Embed epel-release
 yum install -y GeoIP-devel
 yum install -y lua-devel
+yum install -y ruby bison rake
 
+# get sources
 cd /usr/local/src/
-# for ngx_lua
 git clone https://github.com/openresty/lua-nginx-module.git
 git clone https://github.com/simpl/ngx_devel_kit.git
-
+git clone https://github.com/matsumoto-r/ngx_mruby.git
 curl -L -O http://nginx.org/download/nginx-$VERSION.tar.gz
 tar zxvf ./nginx-$VERSION.tar.gz
-cd nginx-$VERSION
-./configure --prefix=/usr/local/nginx --sbin-path=/usr/local/nginx --add-module=/usr/local/src/lua-nginx-module --add-module=/usr/local/src/ngx_devel_kit
+
+# nginx_mruby
+cd ${NGX_MRUBY_SRC}
+git submodule init
+git submodule update
+./configure --with-ngx-src-root=${NGINX_SRC}
+make build_mruby
+make generate_gems_config
+
+# nginx
+cd $NGINX_SRC
+./configure --prefix=/usr/local/nginx --sbin-path=/usr/local/nginx --add-module=/usr/local/src/lua-nginx-module --add-module=/usr/local/src/ngx_devel_kit --add-module=${NGX_MRUBY_SRC}
 make
 sudo make install
+
 echo "export PATH=/usr/local/nginx/:$PATH" >> /etc/profile.d/nginx.sh
 source /etc/profile
 
